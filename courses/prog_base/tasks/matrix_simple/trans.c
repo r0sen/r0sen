0b5810d806d721a37cfe61f98f946e1ff8b0e682
r0sen <medvedskiy.denys@gmail.com>
2015-11-15 18:32:21 +0200

    f
    r

---
+++ courses/prog_base/tasks/matrix_simple/trans.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <math.h>
+int mat2[4][4];
+void fillRand(int mat[4][4])
+{
+    time_t t;
+    srand((unsigned) time(&t));
+    int i,j;
+    for(i=0; i<4; i++)
+        for(j=0; j<4; j++)
+        {
+            mat[i][j]=rand() % 1999 -999;
+        }
+}
+void rotateCCW90(int mat[4][4])
+{
+    int i,j,k,r,temp;
+    int *p;
+    for(i=0; i<4; i++)
+        for(j=0; j<4; j++)
+        {
+            mat2[i][j]=mat[j][3-i];
+        }
+}
+void flipV(int mat[4][4])
+{
+    int i,j;
+    for (i=0; i<4; i++)
+        for(j=0; j<4; j++)
+        {
+            mat2[i][j]=mat[3-i][j];
+        }
+}
+void transposSide(int mat[4][4])
+{
+    int i,j;
+    for (i=0; i<4; i++)
+        for(j=0; j<4; j++)
+        {
+            mat2[i][j]=mat[3-j][3-i];
+        }
+}
