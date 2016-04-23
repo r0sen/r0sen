503c2fb13b479ccb5f4107bc9a554b11fe82f96d
r0sen <medvedskyi.denys@gmail.com>
2016-04-23 13:17:45 +0300

    changes format
    //

--- courses/prog_base_2/labs/lab2/dynLIb/main.cpp
+++ courses/prog_base_2/labs/lab2/dynLIb/main.cpp
@@ -4,12 +4,9 @@ int check_list(list_t * list)
 {
     int i;
     int sum = 0;
-
     for(i = 0; i<list_callSize(list); i++)
         sum+=list_callData(list, i);
-
     printf("Success!");
-
     int a = sum/list_callSize(list);
     return a;
 }
@@ -26,22 +23,22 @@ extern "C" DLL_EXPORT BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason,
 {
     switch (fdwReason)
     {
-        case DLL_PROCESS_ATTACH:
-            // attach to process
-            // return FALSE to fail DLL load
-            break;
+    case DLL_PROCESS_ATTACH:
+        // attach to process
+        // return FALSE to fail DLL load
+        break;
 
-        case DLL_PROCESS_DETACH:
-            // detach from process
-            break;
+    case DLL_PROCESS_DETACH:
+        // detach from process
+        break;
 
-        case DLL_THREAD_ATTACH:
-            // attach to thread
-            break;
+    case DLL_THREAD_ATTACH:
+        // attach to thread
+        break;
 
-        case DLL_THREAD_DETACH:
-            // detach from thread
-            break;
+    case DLL_THREAD_DETACH:
+        // detach from thread
+        break;
     }
     return TRUE; // successful
 }
--- courses/prog_base_2/labs/lab2/dynLIb2/main.cpp
+++ courses/prog_base_2/labs/lab2/dynLIb2/main.cpp
@@ -19,22 +19,22 @@ extern "C" DLL_EXPORT BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason,
 {
     switch (fdwReason)
     {
-        case DLL_PROCESS_ATTACH:
-            // attach to process
-            // return FALSE to fail DLL load
-            break;
+    case DLL_PROCESS_ATTACH:
+        // attach to process
+        // return FALSE to fail DLL load
+        break;
 
-        case DLL_PROCESS_DETACH:
-            // detach from process
-            break;
+    case DLL_PROCESS_DETACH:
+        // detach from process
+        break;
 
-        case DLL_THREAD_ATTACH:
-            // attach to thread
-            break;
+    case DLL_THREAD_ATTACH:
+        // attach to thread
+        break;
 
-        case DLL_THREAD_DETACH:
-            // detach from thread
-            break;
+    case DLL_THREAD_DETACH:
+        // detach from thread
+        break;
     }
     return TRUE; // succesful
 }
